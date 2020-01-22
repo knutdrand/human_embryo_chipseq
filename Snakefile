4faef7f525e7853de1a09714558fe6dbb99bb006
Knut Rand <knutdrand@gmail.com>
2020-01-22 10:00:58 +0100

    added kb hist rule

--- Snakefile
+++ Snakefile
@@ -1,10 +1,50 @@
+rule peak_call_v3:
+    input:
+        "sorted_alignments/{name}_K4.bed"
+        "sorted_alignments/{name}_In.bed"
+    output:
+        "v3/macs_output/{name}_peaks.broadPeak",
+        temp("v3/macs_output/{name}_treat_pileup.bdg"),
+	temp("v3/macs_output/{name}_control_lambda.bdg")
+    shell:
+        "macs2 -t {input[0]} -c {input[1]} --bdg -B -n {wildcards.name} --broad -o v3/peaks/"
+
+rule create_pileup_track:
+    input:
+        "{version}/macs_output/{name}_treat_pileup.bdg"
+    output:
+        "{version}/macs_output/{name}_treat_pileup.bw"
+    shell:
+        "./bdg2bw {input} mm10.chrom.sizes"
+
+
+rule create_peak_track:
+    input:
+        "{version}/macs_output/{name}_peaks.narrowPeak"
+    output:
+        "{version}/macs_output/{name_peaks.bb}"
+    shell:
+        "./narrowPeak2bb.sh {input} mm10.chrom.sizes"
+
+rule merge_peaks:
+    input:
+        "{version}/peaks/{name}.broadPeak"
+    output:
+        "{version}/domains/{name}.bed"
+    shell:
+        "bedtools merge -d 5000 -i {input} > {output}"
+
 rule size_hist:
     input:
         "peaks/{name}_{class}.{type}Peak"
     output:
-        "histograms/{name}_{class}_{type}.png"
+        "histograms/{name}_{class}_{type}.png",
+        "histograms/{name}_{class}_{type}_log.png"
     shell:
-        "python3 src/peak_histograms.py {input} {output}"
+        """
+	python3 src/peak_histograms.py hist {input} {output[0]}
+	python3 src/peak_histograms.py loghist {input} {output[1]}
+	"""
 
 rule copy_domains:
     input:
--- src/peak_histograms.py
+++ src/peak_histograms.py
@@ -3,28 +3,44 @@ import numpy as np
 bin_size = 0.05
 n_bins = 250
 
-def get_hist(lines):
+def get_log_hist(sizes):
     bins = np.zeros(n_bins, dtype="int")
-    parts = (l.split() for l in lines)
-    log_sizes = (np.log(int(p[2])-int(p[1])) for p in parts)
+    log_sizes = (np.log(size) for size in sizes)
     size_bins = (min(s//bin_size, n_bins-1) for s in log_sizes)
     for sb in size_bins:
         bins[int(sb)] += 1
-    return np.exp(np.arange(bins.size)*bin_size), bins
+    N = 110
+    ticks = np.arange(0, bins.size-N, (bins.size-N)//7)
+    tick_values = np.array(np.exp((ticks+N)*bin_size), dtype="int")
+    plt.plot(bins[N:])
+    plt.xticks(ticks, tick_values)
+    return bins
+
+def get_sizes(lines):
+    parts = (l.split() for l in lines)
+    return (int(p[2])-int(p[1]) for p in parts)
     
+def get_kb_hist(sizes):
+    n_bins = 50
+    bins = np.zeros(n_bins, dtype="int")
+    size_bins = (min(s//1000, n_bins) for s in sizes) 
+    for sb in size_bins:
+        bins[int(sb)] += 1
+    plt.plot(bins)
+    return bins
+
+funcs = {"hist": get_kb_hist,
+         "loghist": get_log_hist}
+
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) > 1:
-        input_lines = open(sys.argv[1])
+    if len(sys.argv) > 2:
+        input_lines = open(sys.argv[2])
     else:
         input_lines = sys.stdin
-    out_name = sys.argv[2]
-    x, y = get_hist(input_lines)
-    N = 110 #np.flatnonzero(y)[0]
-    print(N)
-    plt.plot(y[N:])
-    ticks = np.arange(0, y.size-N, (y.size-N)//7)
-    plt.xticks(ticks, np.array(np.exp((ticks+N)*bin_size), dtype="int"))
+    out_name = sys.argv[-1]
+    sizes = get_sizes(input_lines)
+    funcs[sys.argv[1]](sizes)
     plt.xlabel("size")
     plt.ylabel("count")
     plt.savefig(out_name)
